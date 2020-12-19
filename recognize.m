e4a90fcb0643ae8431dda7b5ec04e17faff0958d
zbs9804 <69039296+zbs9804@users.noreply.github.com>
2020-12-19 16:06:08 +0800

    Delete recognize.m

--- recognize.m
+++
@@ -1,6 +0,0 @@
-function accuracy=recognize(net,im)
-res=vl_simplenn(net,im.data);
-disp(res(end));
-scores=squeeze(gather(res(end).x));
-[bestScore,best]=max(scores);
-accuracy=length(find(best==im.label))/length(im.label);
